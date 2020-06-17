eb996cbf54c0f3f44478100f975eab59f0ed2948
surendra172001 <52756184+surendra172001@users.noreply.github.com>
2020-06-17 10:50:49 +0530

    date : 17/6/20

---
+++ 20_6_17_min_chars_required_to_make_str_palidrome.cpp
@@ -0,0 +1,34 @@
+int Solution::solve(string A) {
+    int n = A.length();
+    int flag = true;
+    for(int i = 0; i < n/2; i++) {
+        if(A[i] != A[n-i-1]) {
+            flag = false;
+            break;
+        }
+    }
+    if(flag) return 0;
+    
+    int ans = -1;
+    
+    for(int i = n-1; i > 0; i--) {
+        if(A[0] == A[i]) {
+            int l = 1, r = i-1;
+            int cnt = 2;
+            while(l < r && A[l] == A[r]) {
+                cnt += 2;
+                l++;
+                r--;
+            }
+            
+            //if length is odd include the middle character
+            cnt += (l == r);
+            if(cnt == i+1) {
+                ans = cnt;
+                break;
+            }
+        }
+    }
+    
+    return (ans == -1 ? n-1 : n-ans);    
+}
